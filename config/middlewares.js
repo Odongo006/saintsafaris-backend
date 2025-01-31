6ff370295c09d24f338df3f2c5ba50f5e2e7ab41
Alpha7 <henryomondi6612@gmail.com>
2025-02-01 01:20:22 +0300

    -m

--- config/middlewares.js
+++ config/middlewares.js
@@ -11,7 +11,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:4173', 'http://localhost:5173', 'https://www.saintsafaris.com'], // Include all possible frontend URLs
+      origin: ['http://localhost:4173', 'http://localhost:5173', 'https://www.saintsafaris.com', 'https://saintapi.koyeb.app/'], // Include all possible frontend URLs
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       credentials: true,
     },
--- config/server.js
+++ config/server.js
@@ -2,14 +2,14 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['1BHynDp83Hgq65tXQLpOhg==', 'zj+CSMvhQ8o7FOIKXhat2w==', 'KczFToS2glLqFqFuo/HH7g==', 'iG5zzSpvsM9+yHd0zF90aQ=='], // Add keys manually here
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
   logger: {
     level: 'debug', // Log level
-    exposeInContext: true, // Makes the logger available in the context
+    exposeInContext: true, // Makes the logger available
     requests: true, // Logs HTTP requests
   },
 });
